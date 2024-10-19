2dd2ecd2298256ad7fbb288f9647981dc704e967
Khatibul Luthfi <luthfiandewa@gmail.com>
2024-10-19 16:21:44 +0700

    Update jendela1.py

--- jendela1.py
+++ jendela1.py
@@ -1,4 +1,4 @@
-import pySimpleGUI as sg
-window = sg.window(tittle="Latihan Pertama",layout=[[],size=(400,200)])
+import PySimpleGUI as sg
+window = sg.Window(title="Latihan pertama",layout=[[sg.Text("Selamat belajar PysimpleGUI Luthfi ")]],size=(400,200))
 window()
 window.close()
